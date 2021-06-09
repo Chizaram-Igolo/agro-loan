032f8fb56f801c783d0f77da84fa9bb517945dfb
Chizaram-Igolo <chizaram.igolo@yahoo.com>
2021-06-09 13:49:55 +0100

    add heroku clear db config

--- includes/constants.php
+++ includes/constants.php
@@ -1,8 +1,12 @@
 <?php
-    DEFINE ("DB_HOST", "localhost");
-    DEFINE ("DB_USER", "root");
-    DEFINE ("DB_PASS", ""); 
-    DEFINE ("DB_NAME", "agro_loan_db");
+    $db_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    DEFINE ("DB_HOST", $db_url["host"]);
+    DEFINE ("DB_USER", $db_url["user"]);
+    DEFINE ("DB_PASS", $db_url["pass"]); 
+    DEFINE ("DB_NAME", substr($db_url["path"], 1));
+
+    $active_group = 'default';
+    $query_builder = TRUE;
 	
 	$Company_Name = "DERDC";
 	DEFINE ("COMPANY_NAME", $Company_Name);
